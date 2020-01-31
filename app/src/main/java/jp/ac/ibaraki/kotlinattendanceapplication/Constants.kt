31d1d9d57997998b161f8b0989a9bea9c9e36f48
TeamKamadaKinoshita2020 <60334804+TeamKamadaKinoshita2020@users.noreply.github.com>
2020-01-31 12:52:21 +0900

    Update Constants.kt
    固有の値を汎用的な内容に変更

--- app/src/main/java/jp/ac/ibaraki/kotlinattendanceapplication/Constants.kt
+++ app/src/main/java/jp/ac/ibaraki/kotlinattendanceapplication/Constants.kt
@@ -8,8 +8,7 @@ class Constants {
      * 通信関連の定数
      */
     companion object {
-        const val BASE_URL = "http://13.114.37.66/attendancesystem/android/" // aws
-        // const val BASE_URL = " http://157.80.87.65/attendancesystem/android/" // localhost
+        const val BASE_URL = "hoge" // "http:/IP address/attendancesystem/android/"
         const val GET_POSSIBLE_ATTEND_URL = "get-possible-attend-list.php/"
         const val CHECK_ALREADY_ATTEND_URL = "check-already-attend.php/"
         const val GET_SEAT_NUM_URL = "get-seat-num.php/"
